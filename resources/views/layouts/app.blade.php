7dd740d8c2266a082cd7213e715af933bde00400
Jafar Aziz <aziz@jafaraziz.com>
2021-10-09 05:19:26 +0700

    update html metadata

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    
+    <title>Impor Jawa Timur - Jafar Husaini Aziz</title>
+    <meta name="description" content="Cuma gabut doang sih, buat ginian">
+
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @livewireStyles
