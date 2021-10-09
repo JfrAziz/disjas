a28732df721675c0edfbac756a01b8db024e1ed6
Jafar Aziz <aziz@jafaraziz.com>
2021-10-10 02:02:59 +0700

    add my name & just make computer beep boop

--- resources/views/components/separator.blade.php
+++ resources/views/components/separator.blade.php
@@ -1,6 +1,5 @@
-{{-- border bottom --}}
-<div class="hidden sm:block">
-    <div class="pb-5">
+<div class="sm:block">
+    <div class="pb-5 pt-3">
         <div class="border-t border-gray-200"></div>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,15 @@
 <x-app-layout>
+    <x-card>
+        <div class="md:flex md:items-center md:justify-between text-gray-700">
+            <div class="text-xl font-bold ">
+                Jafar Husaini Aziz
+            </div>
+            <div class="text-base mt-2 md:m-0">
+                Statistik Distribusi & Jasa
+            </div>
+        </div>
+    </x-card>
+    <x-separator />
     <x-card title="Impor Jawa Timur">
         @slot('aside')
             @livewire('bar-race')
